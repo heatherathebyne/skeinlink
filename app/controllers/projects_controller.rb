63d305a10282635dc91af16175931919ae3e7a4a
Heather Athebyne <heather@athebyne.com>
2019-11-10 06:40:22 -0500

    Visual improvements

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.all.order(id: :desc)
   end
 
   # GET /projects/1
