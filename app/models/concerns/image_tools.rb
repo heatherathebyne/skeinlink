8c5a6686fa1cf2886779ba2b65fc1f6935eea884
Heather Athebyne <heather@athebyne.com>
2020-01-04 12:48:57 -0500

    Automatically resize uploaded project images

---
+++ app/models/concerns/image_tools.rb
@@ -0,0 +1,22 @@
+THUMBNAIL_VARIANT_OPTIONS = { resize_to_limit: [250, 250], strip: true }.freeze
+FULL_SIZE_VARIANT_OPTIONS = { resize_to_limit: [1280, 1280], strip: true }.freeze
+
+module ImageTools
+  extend ActiveSupport::Concern
+
+  included do
+    def thumb_and_full_images
+      images.map do |image|
+        { thumb: image.variant(**THUMBNAIL_VARIANT_OPTIONS), full: image.variant(**FULL_SIZE_VARIANT_OPTIONS) }
+      end
+    end
+
+    private
+
+    def replace_image_filenames
+      images.each do |image|
+        image.blob.filename = SecureRandom.hex(8) # This is only persisted on new images
+      end
+    end
+  end
+end
