3696af5cfbb28d5c3d9f406de0fdd68bb6223807
Heather Athebyne <heather@athebyne.com>
2020-01-12 13:49:03 -0500

    Put "add another whatever" buttons on more #show pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def current_users_project?
     @project.user_id == current_user.id
   end
+
+  def belongs_to_current_user?(something)
+    return false unless current_user and something.user_id
+    current_user.id == something.user_id
+  end
 end
