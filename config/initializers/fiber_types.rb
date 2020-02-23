6472a7cb9ca5706e66ec762efd27354239628f17
Heather Athebyne <heather@athebyne.com>
2020-02-23 09:34:30 -0500

    [#30] Yarn database tags can be added and removed

---
+++ config/initializers/fiber_types.rb
@@ -0,0 +1,56 @@
+FIBER_TYPES = {
+  'Alpaca' => [
+    'Baby Alpaca',
+    'Huacaya Alpaca',
+    'Superfine Alpaca',
+    'Suri Alpaca'
+  ],
+  'Angora' => [],
+  'Camel' => [],
+  'Cashmere' => [],
+  'Cotton' => [],
+  'Hemp' => [],
+  'Linen' => [
+    'Flax'
+  ],
+  'Llama' => [
+    'Baby Llama'
+  ],
+  'Mohair' => [
+    'Kid Mohair'
+  ],
+  'Qiviut' => [],
+  'Silk' => [
+    'Muga Silk',
+    'Mulberry Silk',
+    'Silk Hankies (Mawata)',
+    'Silk Noil',
+    'Tussah Silk'
+  ],
+  'Synthetic' => [
+    'Acrylic',
+    'Metallic',
+    'Nylon',
+    'Other Synthetic',
+    'Polyamide',
+    'Polyester',
+    'Polypropylene'
+  ],
+  'Rayon (Viscose)' => [
+    'Bamboo Rayon (Viscose)',
+    'Mint Rayon (Viscose)',
+    'Pearl Rayon (Viscose)',
+    'Tencel',
+    'Seacell'
+  ],
+  'Wool' => [
+    'Bluefaced Leicester Wool',
+    'Cormo Wool',
+    'Merino Wool',
+    'Rambouillet Wool',
+    'Shetland Wool',
+    'Superwash Wool',
+    'Zwartbles Wool'
+  ],
+  'Yak' => []
+}.freeze
