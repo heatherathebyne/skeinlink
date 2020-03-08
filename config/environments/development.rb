ca3a88450162dfcabd346786c61991d5aeda500b
Heather Athebyne <heather@athebyne.com>
2020-03-08 08:05:52 -0400

    Allow log level to be specified in env vars

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.log_level = ENV['LOG_LEVEL'].in?(%w(debug info warn error fatal unknown)) ? ENV['LOG_LEVEL'].to_sym : :debug
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  #config.log_level = :info
+  config.log_level = ENV['LOG_LEVEL'].in?(%w(debug info warn error fatal unknown)) ? ENV['LOG_LEVEL'].to_sym : :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
