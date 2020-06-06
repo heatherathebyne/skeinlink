e3135d3e9380f9f2ebc1e84b90260ee36ac18461
Heather Athebyne <heather@athebyne.com>
2020-06-06 10:08:33 -0400

    [#55] Allow creation of new yarn product during stash yarn creation

--- app/models/yarn_product.rb
+++ app/models/yarn_product.rb
@@ -10,7 +10,7 @@ class YarnProduct < ApplicationRecord
   belongs_to :yarn_company, optional: true # y tho
   has_many :colorways, inverse_of: :yarn_product
   has_one_attached :image
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :image, content_type: { in: [:png, :jpg, :jpeg, :gif], message: 'is not a PNG, JPG, or GIF image' },
                      size: { less_than: 15.megabytes, message: "Whoa, that image is too big! Try one that is smaller than 15 MB." }
 
