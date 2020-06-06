48f3d712d135476125c89d61d22c4e7a4e5263a3
Heather Athebyne <heather@athebyne.com>
2020-06-06 13:15:28 -0400

    [#56] Colorways can be added while creating new stash yarn

--- app/controllers/colorways_controller.rb
+++ app/controllers/colorways_controller.rb
@@ -17,10 +17,22 @@ class ColorwaysController < ApplicationController
 
     ImageAttachmentService.call(record: @colorway, images: colorway_image_params[:image])
 
-    if @colorway.save
-      redirect_to @yarn_product, notice: 'Colorway added!'
-    else
-      redirect_to @yarn_product, alert: "We're sorry, but we couldn't create that colorway: #{@colorway.errors.messages}"
+    respond_to do |format|
+      format.html do
+        if @colorway.save
+          redirect_to @yarn_product, notice: 'Colorway added!'
+        else
+          redirect_to @yarn_product, alert: "We're sorry, but we couldn't create that colorway: #{@colorway.errors.messages}"
+        end
+      end
+
+      format.js do
+        if @colorway.save
+          render status: :ok, json: { id: @colorway.id, name_with_number: @colorway.name_with_number }
+        else
+          render status: :not_acceptable, json: { errors: @colorway.errors.full_messages }
+        end
+      end
     end
   end
 
@@ -52,6 +64,16 @@ class ColorwaysController < ApplicationController
     render :index
   end
 
+  def autocomplete_name_or_number_for_stash
+    @colorways = Colorway.where(yarn_product_id: params[:yarn_product_id])
+                         .where("name LIKE ? OR number LIKE ?",
+                                "%#{params[:term]}%",
+                                "%#{params[:term]}%")
+                                  .limit(6).order(name: :asc).to_a
+    @colorways << Colorway.new(id: -3, name: 'Tell us about another colorway', number: '')
+    render :index
+  end
+
   private
 
   def colorway_params
@@ -67,6 +89,7 @@ class ColorwaysController < ApplicationController
   end
 
   def set_yarn_product
-    @yarn_product = YarnProduct.includes(:yarn_company).find(params[:yarn_product_id])
+    yarn_product_id = params[:yarn_product_id] || params[:colorway][:yarn_product_id]
+    @yarn_product = YarnProduct.includes(:yarn_company).find(yarn_product_id || nil)
   end
 end
