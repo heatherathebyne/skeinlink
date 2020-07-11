fad757fd6f50319cf60ef7e9060136f303ebbc13
Heather Athebyne <heather@athebyne.com>
2020-07-11 08:43:45 -0400

    [#33] Add sorting scopes to YarnProduct

--- app/models/yarn_product.rb
+++ app/models/yarn_product.rb
@@ -10,9 +10,23 @@ class YarnProduct < ApplicationRecord
   belongs_to :yarn_company, optional: true # y tho
   has_many :colorways, inverse_of: :yarn_product
   has_one_attached :image
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :image, content_type: { in: [:png, :jpg, :jpeg, :gif], message: 'is not a PNG, JPG, or GIF image' },
                      size: { less_than: 15.megabytes, message: "Whoa, that image is too big! Try one that is smaller than 15 MB." }
+
+  scope :newest, -> { order id: :desc }
+  scope :oldest, -> { order id: :asc }
+  scope :yarn_name_a_z, -> { order name: :asc }
+  scope :yarn_name_z_a, -> { order name: :desc }
+  scope :yarn_maker_name_a_z, -> {
+    left_outer_joins(:yarn_company).includes(:yarn_company)
+      .order('yarn_companies.name ASC, yarn_products.name ASC')
+  }
+  scope :yarn_maker_name_z_a, -> {
+    left_outer_joins(:yarn_company).includes(:yarn_company)
+      .order('yarn_companies.name DESC, yarn_products.name DESC')
+  }
 
   def name_with_company
     if yarn_company&.name
