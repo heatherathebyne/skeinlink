22753054ebb76b0c8ebf1f6e761bcf0fe1ab17df
Heather Athebyne <heather@athebyne.com>
2020-07-11 07:31:55 -0400

    [#33] Add scope to sort stash yarns by name or yarn product name

--- app/models/stash_yarn.rb
+++ app/models/stash_yarn.rb
@@ -11,11 +11,87 @@ class StashYarn < ApplicationRecord
   belongs_to :colorway, optional: true
   belongs_to :user
   has_one_attached :image
+
   validate :has_a_name_or_product
   validates :image, content_type: { in: [:png, :jpg, :jpeg, :gif], message: 'is not a PNG, JPG, or GIF image' },
                     size: { less_than: 15.megabytes, message: "Whoa, that image is too big! Try one that is smaller than 15 MB." }
   validates :other_maker_type, inclusion: { in: 0..2 }, allow_nil: true
   validates :dye_lot, length: { maximum: 255 }
+
+  scope :newest, -> { order id: :desc }
+  scope :oldest, -> { order id: :asc }
+
+  scope :yarn_name_a_z, -> {
+    select(
+      'stash_yarns.id, stash_yarns.yarn_product_id, stash_yarns.colorway_id, stash_yarns.user_id, '\
+      'stash_yarns.name, stash_yarns.colorway_name, stash_yarns.purchase_date, '\
+      'stash_yarns.purchased_at_name, stash_yarns.purchase_price, stash_yarns.skein_quantity, '\
+      'stash_yarns.total_yardage, stash_yarns.created_at, stash_yarns.updated_at, '\
+      'stash_yarns.notes, stash_yarns.handspun, stash_yarns.weight_id, '\
+      'stash_yarns.other_maker_type, stash_yarns.dye_lot, '\
+      '(CASE WHEN stash_yarns.yarn_product_id IS NULL THEN stash_yarns.name ELSE yarn_products.name END) AS name'
+    )
+      .from('stash_yarns LEFT OUTER JOIN yarn_products ON yarn_products.id = stash_yarns.yarn_product_id')
+      .includes({ yarn_product: :yarn_company })
+      .includes(:colorway)
+      .with_attached_image
+      .order('name ASC') # this exact string ensures we count yarn product name in the sort
+  }
+
+  scope :yarn_name_z_a, -> {
+    select(
+      'stash_yarns.id, stash_yarns.yarn_product_id, stash_yarns.colorway_id, stash_yarns.user_id, '\
+      'stash_yarns.name, stash_yarns.colorway_name, stash_yarns.purchase_date, '\
+      'stash_yarns.purchased_at_name, stash_yarns.purchase_price, stash_yarns.skein_quantity, '\
+      'stash_yarns.total_yardage, stash_yarns.created_at, stash_yarns.updated_at, '\
+      'stash_yarns.notes, stash_yarns.handspun, stash_yarns.weight_id, '\
+      'stash_yarns.other_maker_type, stash_yarns.dye_lot, '\
+      '(CASE WHEN stash_yarns.yarn_product_id IS NULL THEN stash_yarns.name ELSE yarn_products.name END) AS name'
+    )
+      .from('stash_yarns LEFT OUTER JOIN yarn_products ON yarn_products.id = stash_yarns.yarn_product_id')
+      .includes({ yarn_product: :yarn_company })
+      .includes(:colorway)
+      .with_attached_image
+      .order('name DESC') # this exact string ensures we count yarn product name in the sort
+  }
+
+  # Sort first by maker name, then by yarn product name,
+  # interleaving stash without yarn product as if it was a maker name
+  scope :yarn_maker_name_a_z, -> {
+    select(
+      'stash_yarns.id, stash_yarns.yarn_product_id, stash_yarns.colorway_id, stash_yarns.user_id, '\
+      'stash_yarns.name, stash_yarns.colorway_name, stash_yarns.purchase_date, '\
+      'stash_yarns.purchased_at_name, stash_yarns.purchase_price, stash_yarns.skein_quantity, '\
+      'stash_yarns.total_yardage, stash_yarns.created_at, stash_yarns.updated_at, '\
+      'stash_yarns.notes, stash_yarns.handspun, stash_yarns.weight_id, '\
+      'stash_yarns.other_maker_type, stash_yarns.dye_lot, '\
+      '(CASE WHEN stash_yarns.yarn_product_id IS NULL THEN stash_yarns.name ELSE yarn_products.name END) AS name, '\
+      '(CASE WHEN stash_yarns.yarn_product_id IS NULL THEN stash_yarns.name ELSE yarn_companies.name END) AS maker_name'
+    )
+      .from('stash_yarns LEFT OUTER JOIN yarn_products ON yarn_products.id = stash_yarns.yarn_product_id LEFT OUTER JOIN yarn_companies ON yarn_companies.id = yarn_products.yarn_company_id')
+      .includes({ yarn_product: :yarn_company })
+      .includes(:colorway)
+      .with_attached_image
+      .order('maker_name ASC, name ASC') # this exact string ensures we count yarn product name in the sort
+  }
+
+  scope :yarn_maker_name_z_a, -> {
+    select(
+      'stash_yarns.id, stash_yarns.yarn_product_id, stash_yarns.colorway_id, stash_yarns.user_id, '\
+      'stash_yarns.name, stash_yarns.colorway_name, stash_yarns.purchase_date, '\
+      'stash_yarns.purchased_at_name, stash_yarns.purchase_price, stash_yarns.skein_quantity, '\
+      'stash_yarns.total_yardage, stash_yarns.created_at, stash_yarns.updated_at, '\
+      'stash_yarns.notes, stash_yarns.handspun, stash_yarns.weight_id, '\
+      'stash_yarns.other_maker_type, stash_yarns.dye_lot, '\
+      '(CASE WHEN stash_yarns.yarn_product_id IS NULL THEN stash_yarns.name ELSE yarn_products.name END) AS name, '\
+      '(CASE WHEN stash_yarns.yarn_product_id IS NULL THEN stash_yarns.name ELSE yarn_companies.name END) AS maker_name'
+    )
+      .from('stash_yarns LEFT OUTER JOIN yarn_products ON yarn_products.id = stash_yarns.yarn_product_id LEFT OUTER JOIN yarn_companies ON yarn_companies.id = yarn_products.yarn_company_id')
+      .includes({ yarn_product: :yarn_company })
+      .includes(:colorway)
+      .with_attached_image
+      .order('maker_name DESC, name DESC') # this exact string ensures we count yarn product name in the sort
+  }
 
   def name
     yarn_product.try(:name_with_company) || super
