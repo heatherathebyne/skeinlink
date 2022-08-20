fef42b057ea875f4449c42296a40870e5129a2f9
Heather Athebyne <heather@athebyne.com>
2022-08-20 09:11:45 -0400

    6.1 config defaults

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Skeinlink
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Include site-specific configuration
     begin
